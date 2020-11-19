2ce9e04be5e4cdd3b6aa67ceaba784d56414558d
Ivar Refsdal <ivar.refsdal@nsd.no>
2020-11-19 13:07:37 +0100

    Fix rewrite-hist-test

--- test/no/nsd/rewrite_hist_test.clj
+++ test/no/nsd/rewrite_hist_test.clj
@@ -16,28 +16,34 @@
    :log-to-elk false})
 
 (defn setup! [conn]
-  @(d/transact conn #d/schema[[:m/id :one :string :id]
-                              [:m/info :one :string]
-                              [:m/address :one :ref :component]
-                              [:m/vedlegg :many :ref :component]
-                              [:m/type :one :ref]
-                              [:type/standard :enum]
-                              [:type/special :enum]
-                              [:vedlegg/id :one :string :id]
-                              [:vedlegg/info :one :string]
-                              [:addr/country :one :ref :component]
-                              [:country/name :one :string :id]
-                              [:country/region :one :string]])
+  @(d/transact conn (conj #d/schema[[:m/id :one :string :id]
+                                    [:m/info :one :string]
+                                    [:m/address :one :ref :component]
+                                    [:m/vedlegg :many :ref :component]
+                                    [:m/type :one :ref]
+                                    [:type/standard :enum]
+                                    [:type/special :enum]
+                                    [:vedlegg/id :one :string :id]
+                                    [:vedlegg/info :one :string]
+                                    [:addr/country :one :ref :component]
+                                    [:country/name :one :string :id]
+                                    [:country/region :one :string]]
+                          {:db/id        "datomic.tx"
+                           :db/txInstant #inst"1999"}))
 
   @(d/transact conn [{:m/id      "id-1"
                       :m/address {:addr/country
                                   {:country/name   "Norway"
-                                   :country/region "West Europe"}}}])
+                                   :country/region "West Europe"}}}
+                     {:db/id        "datomic.tx"
+                      :db/txInstant #inst"2000"}])
 
   @(d/transact conn [{:m/id      "id-1"
                       :m/address {:addr/country
                                   {:country/name   "Norway"
-                                   :country/region "Europe"}}}]))
+                                   :country/region "Europe"}}}
+                     {:db/id        "datomic.tx"
+                      :db/txInstant #inst"2001"}]))
 
 (deftest rewrite-hist-test
   (let [conn (u/empty-conn)
@@ -45,22 +51,25 @@
         db (d/db conn)
         fh (impl/pull-flat-history-simple db [:m/id "id-1"])
         txes (impl/history->transactions db fh)]
-    (is (= [[[:db/add "1" :m/address "2"]
-             [:db/add "1" :m/id "id-1"]
-             [:db/add "2" :addr/country "3"]
-             [:db/add "3" :country/name "Norway"]
-             [:db/add "3" :country/region "West Europe"]]
+    (sc/spy!)
+    (is (= [[[:db/add "datomic.tx" :db/txInstant2 #inst"2000"]
+             [:db/add "3" :m/address "4"]
+             [:db/add "3" :m/id "id-1"]
+             [:db/add "4" :addr/country "5"]
+             [:db/add "5" :country/name "Norway"]
+             [:db/add "5" :country/region "West Europe"]]
 
-            [[:db/retract [:tempid "1"] :m/address [:tempid "2"]]
-             [:db/add [:tempid "1"] :m/address "4"]
-             [:db/retract [:tempid "3"] :country/region "West Europe"]
-             [:db/add [:tempid "3"] :country/region "Europe"]
-             [:db/add "4" :addr/country [:tempid "3"]]]]
+            [[:db/add "datomic.tx" :db/txInstant2 #inst"2001"]
+             [:db/retract [:tempid "3"] :m/address [:tempid "4"]]
+             [:db/add [:tempid "3"] :m/address "6"]
+             [:db/retract [:tempid "5"] :country/region "West Europe"]
+             [:db/add [:tempid "5"] :country/region "Europe"]
+             [:db/add "6" :addr/country [:tempid "5"]]]]
            txes))
     @(d/transact conn [[:db.fn/retractEntity [:m/id "id-1"]]])
     (try
       (timbre/with-merged-config {:min-level :fatal}
-        (impl/pull-flat-history (d/db conn) [:m/id "id-1"]))
+                                 (impl/pull-flat-history (d/db conn) [:m/id "id-1"]))
       (assert false "should not get here")
       (catch Exception e
         (is (= "Could not find lookup ref" (.getMessage e)))))
