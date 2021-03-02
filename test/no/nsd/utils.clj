1c14cc1ddc159352ab670eb89eaa9eb6ab375018
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-02 10:57:40 +0100

    Support loops / graphs

--- test/no/nsd/utils.clj
+++ test/no/nsd/utils.clj
@@ -1,7 +1,10 @@
 (ns no.nsd.utils
   (:require [datomic.api :as d]
             [datomic-schema.core]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint])
+  (:import (java.time.format DateTimeFormatter)
+           (java.time LocalDate ZoneId)
+           (java.util Date)))
 
 (defn empty-conn []
   (let [uri "datomic:mem://hello-world"]
@@ -9,6 +12,18 @@
     (d/create-database uri)
     (let [conn (d/connect uri)]
       conn)))
+
+(defn year->Date [yr]
+  (Date/from
+    (-> (LocalDate/parse (str yr "-01-01") (DateTimeFormatter/ofPattern "yyyy-mm-DD"))
+        (.atStartOfDay (ZoneId/of "UTC"))
+        (.toInstant))))
+
+(defn tx-fn! [conn]
+  (let [yr (atom 1970)]
+    (fn [data]
+      @(d/transact conn (conj data {:db/id "datomic.tx"
+                                    :db/txInstant (year->Date (swap! yr inc))})))))
 
 (defn pprint [x]
   (pprint/pprint x)
