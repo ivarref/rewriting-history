19994ce93a0e2a8f024d0816958e1e83e19605bd
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-02 09:31:38 +0100

    Add failing test for regular reference

---
+++ test/no/nsd/restrictions_test.clj
@@ -0,0 +1,24 @@
+(ns no.nsd.restrictions-test
+  (:require [clojure.test :refer :all]
+            [no.nsd.utils :as u]
+            [datomic.api :as d]
+            [no.nsd.rewriting-history :as rh]
+            [clojure.pprint :refer [pprint]]
+            [datomic-schema.core]))
+
+(deftest regular-ref-test
+  (testing "Verify that a regular reference fails"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (conj #d/schema[[:m/id :one :string :id]
+                                        [:m/person :one :ref]
+                                        [:person/name :one :string]]
+                              {:db/id        "datomic.tx"
+                               :db/txInstant #inst"1999"}))
+      @(d/transact conn [{:m/id     "id"
+                          :m/person {:db/id "person"
+                                     :person/name "Børre"}}
+                         {:db/id        "datomic.tx"
+                          :db/txInstant #inst"2000"}])
+      @(d/transact conn #d/schema[[:db/txInstant2 :one :instant]])
+
+      (is (thrown? Throwable (rh/pull-flat-history conn [:m/id "id"]))))))
