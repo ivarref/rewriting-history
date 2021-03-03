9a3d91ea0cf1fc474faaa92820a2b67a9a1c8182
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-03 09:41:47 +0100

    Fix rationale test

--- test/no/nsd/rationale_test.clj
+++ test/no/nsd/rationale_test.clj
@@ -14,47 +14,46 @@
                             [?e _ ?v _ _]]
                           (d/history (d/db conn)))))
 
-(defn demo-rationale [stage-uri]
-  (let [conn (u/empty-stage-conn "rationale-demo-test-1")]
-    @(d/transact conn #d/schema[[:m/id :one :string :id]
-                                [:m/info :one :string]])
-    @(d/transact conn [{:m/id "id" :m/info "original-data"}])
-    @(d/transact conn [{:m/id "id" :m/info "bad-data"}])
-    @(d/transact conn [{:m/id "id" :m/info "good-data"}])
+(defn demo-rationale [conn]
+  @(d/transact conn #d/schema[[:m/id :one :string :id]
+                              [:m/info :one :string]])
+  @(d/transact conn [{:m/id "id" :m/info "original-data"}])
+  @(d/transact conn [{:m/id "id" :m/info "bad-data"}])
+  @(d/transact conn [{:m/id "id" :m/info "good-data"}])
 
-    ; Verify that all expected data is present in the database:
-    (is (contains? (db-values-set conn) "original-data"))
-    (is (contains? (db-values-set conn) "bad-data"))
-    (is (contains? (db-values-set conn) "good-data"))
+  ; Verify that all expected data is present in the database:
+  (is (contains? (db-values-set conn) "original-data"))
+  (is (contains? (db-values-set conn) "bad-data"))
+  (is (contains? (db-values-set conn) "good-data"))
 
-    ; Excise database up to (but not including) the good data.
-    ; Find entity id and exicision point:
-    (let [[eid t] (d/q '[:find [?e ?t]
-                         :in $
-                         :where
-                         [?e :m/id "id" _ _ _]
-                         [?e :m/info "good-data" ?t true]]
-                       (d/history (d/db conn)))]
-      ; Do excision:
-      (let [{:keys [db-after]} @(d/transact conn [{:db/excise         eid
-                                                   :db.excise/attrs   [:m/info]
-                                                   :db.excise/beforeT t}])]
-        ; Sync:
-        @(d/sync-excise conn (d/basis-t db-after))
+  ; Excise database up to (but not including) the good data.
+  ; Find entity id and exicision point:
+  (let [[eid t] (d/q '[:find [?e ?t]
+                       :in $
+                       :where
+                       [?e :m/id "id" _ _ _]
+                       [?e :m/info "good-data" ?t true]]
+                     (d/history (d/db conn)))]
+    ; Do excision:
+    (let [{:keys [db-after]} @(d/transact conn [{:db/excise         eid
+                                                 :db.excise/attrs   [:m/info]
+                                                 :db.excise/beforeT t}])]
+      ; Sync:
+      @(d/sync-excise conn (d/basis-t db-after))
 
-        ; Original data is deleted:
-        (is (false? (contains? (db-values-set conn) "original-data")))
+      ; Original data is deleted:
+      (is (false? (contains? (db-values-set conn) "original-data")))
 
-        ; The bad data is still present in the history database as a retraction!
-        ; This is the problem, and I consider it a bug in Datomic:
-        (is (contains? (db-values-set conn) "bad-data"))
+      ; The bad data is still present in the history database as a retraction!
+      ; This is the problem, and I consider it a bug in Datomic:
+      (is (contains? (db-values-set conn) "bad-data"))
 
-        ; The good data is still present as well:
-        (is (contains? (db-values-set conn) "good-data"))))))
+      ; The good data is still present as well:
+      (is (contains? (db-values-set conn) "good-data")))))
 
 (deftest rationale-test
   (testing "Demonstrate rationale"
-    (if-let [stage-uri (u/stage-uri)]
-      (demo-rationale stage-uri)
+    (if-let [conn (u/empty-stage-conn "rationale-demo-test-1")]
+      (demo-rationale conn)
       (do (log/info "not demonstrating rationale (requires real database uri)")
           (is (= 1 1))))))
