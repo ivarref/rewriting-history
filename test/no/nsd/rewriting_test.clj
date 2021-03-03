a77851566d78de766930b1cc6611b17570692a7e
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-03 13:18:32 +0100

    Remove duplicate test

--- test/no/nsd/rewriting_test.clj
+++ test/no/nsd/rewriting_test.clj
@@ -2,18 +2,11 @@
   (:require [clojure.test :refer :all]
             [datomic.api :as d]
             [datomic-schema.core :as ds]
-            [no.nsd.rewriting-history :as rh]
-            [no.nsd.envelope :as envelope]
             [no.nsd.utils :as u]
             [no.nsd.rewriting-history.impl :as impl]
+            [no.nsd.log-init]
             [no.nsd.shorter-stacktrace]
-            [no.nsd.spy :as sc]
             [clojure.pprint :as pprint]))
-
-(envelope/init!
-  {:min-level  [[#{"datomic.*" "com.datomic.*" "org.apache.*"} :warn]
-                [#{"*"} :info]]
-   :log-to-elk false})
 
 (def schema1 (conj #d/schema[[:m/id :one :string :id]
                              [:m/info :one :string]
@@ -120,15 +113,14 @@
 (deftest history->txes-test
   (let [conn (u/empty-conn)]
     @(d/transact conn schema2)
-    (get-in @(d/transact conn [{:db/id     "entity"
-                                :m/id      "id-1"
-                                :m/type    :type/standard
-                                :m/address {:addr/country
-                                            {:country/name   "Norway"
-                                             :country/region "West Europe"}}}
-                               {:db/id        "datomic.tx"
-                                :db/txInstant #inst"2000"}])
-            [:tempids "entity"])
+    @(d/transact conn [{:db/id     "entity"
+                        :m/id      "id-1"
+                        :m/type    :type/standard
+                        :m/address {:addr/country
+                                    {:country/name   "Norway"
+                                     :country/region "West Europe"}}}
+                       {:db/id        "datomic.tx"
+                        :db/txInstant #inst"2000"}])
     (let [hist (impl/pull-flat-history-simple conn [:m/id "id-1"])
           [tx] (impl/history->transactions conn hist)]
       (is (= [[:db/add "datomic.tx" :db/txInstant2 #inst"2000"]
@@ -139,42 +131,6 @@
               [:db/add "4" :country/name "Norway"]
               [:db/add "4" :country/region "West Europe"]]
              tx))
-      (let [conn (u/empty-conn)]
-        @(d/transact conn schema2)
-        @(d/transact conn tx)
-        (is (= hist (impl/pull-flat-history-simple conn [:m/id "id-1"])))))))
-
-(deftest history->txes-test-unsimplified
-  (let [conn (u/empty-conn)]
-    @(d/transact conn (conj #d/schema[[:m/id :one :string :id]
-                                      [:m/info :one :string]
-                                      [:m/address :one :ref :component]
-                                      [:m/vedlegg :many :ref :component]
-                                      [:m/type :one :ref]
-                                      [:type/standard :enum]
-                                      [:type/special :enum]
-                                      [:vedlegg/id :one :string :id]
-                                      [:vedlegg/info :one :string]
-                                      [:addr/country :one :ref :component]
-                                      [:country/name :one :string :id]
-                                      [:country/region :one :string]]
-                            {:db/id        "datomic.tx"
-                             :db/txInstant #inst"1999"}))
-
-    @(d/transact conn [{:db/id     "entity"
-                        :m/id      "id-1"
-                        :m/type    :type/standard
-                        :m/address {:addr/country
-                                    {:country/name   "Norway"
-                                     :country/region "West Europe"}}}
-                       {:db/id "datomic.tx"
-                        :db/txInstant #inst"2000"}])
-    @(d/transact conn #d/schema[[:db/txInstant2 :one :instant]])
-
-    (let [hist (impl/pull-flat-history conn [:m/id "id-1"])
-          [tx] (impl/history->transactions conn hist)]
-      @(d/transact conn [[:db.fn/retractEntity [:m/id "id-1"]]])
-      @(d/transact conn tx)
       (is (= [[1 :db/txInstant2 #inst"2000" 1 true]
               [2 :m/address 3 1 true]
               [2 :m/id "id-1" 1 true]
@@ -182,4 +138,9 @@
               [3 :addr/country 4 1 true]
               [4 :country/name "Norway" 1 true]
               [4 :country/region "West Europe" 1 true]]
-             (impl/pull-flat-history-simple conn [:m/id "id-1"]))))))
+             hist))
+      (let [conn (u/empty-conn)]
+        @(d/transact conn schema2)
+        @(d/transact conn tx)
+        (is (= hist (impl/pull-flat-history-simple conn [:m/id "id-1"])))))))
+
