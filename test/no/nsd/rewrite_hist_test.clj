f2fdb38fc2f859e8beb1487bd4fbe7f4977be2b1
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-03 13:06:19 +0100

    Clean up test

--- test/no/nsd/rewrite_hist_test.clj
+++ test/no/nsd/rewrite_hist_test.clj
@@ -1,37 +1,32 @@
 (ns no.nsd.rewrite-hist-test
   (:require [clojure.test :refer :all]
-            [datomic-schema.core :as ds]
+            [datomic-schema.core]
             [no.nsd.utils :as u]
-            [no.nsd.envelope :as envelope]
-            [no.nsd.spy :as sc]
+            [no.nsd.log-init]
             [datomic.api :as d]
             [no.nsd.rewriting-history :as rh]
             [no.nsd.shorter-stacktrace]
-            [no.nsd.rewriting-history.impl :as impl]
-            [taoensso.timbre :as timbre]))
+            [no.nsd.rewriting-history.impl :as impl]))
 
-(envelope/init!
-  {:min-level  [[#{"datomic.*" "com.datomic.*" "org.apache.*"} :warn]
-                [#{"*"} :info]]
-   :log-to-elk false})
+(def schema (conj #d/schema[[:m/id :one :string :id]
+                            [:m/info :one :string]
+                            [:m/address :one :ref :component]
+                            [:m/vedlegg :many :ref :component]
+                            [:m/type :one :ref]
+                            [:type/standard :enum]
+                            [:type/special :enum]
+                            [:vedlegg/id :one :string :id]
+                            [:vedlegg/info :one :string]
+                            [:addr/country :one :ref :component]
+                            [:country/name :one :string :id]
+                            [:country/region :one :string]
+                            [:db/txInstant2 :one :instant]]
+                  {:db/id        "datomic.tx"
+                   :db/txInstant #inst"1999"}))
 
 (defn setup-conn []
   (let [conn (u/empty-conn)]
-    @(d/transact conn (conj #d/schema[[:m/id :one :string :id]
-                                      [:m/info :one :string]
-                                      [:m/address :one :ref :component]
-                                      [:m/vedlegg :many :ref :component]
-                                      [:m/type :one :ref]
-                                      [:type/standard :enum]
-                                      [:type/special :enum]
-                                      [:vedlegg/id :one :string :id]
-                                      [:vedlegg/info :one :string]
-                                      [:addr/country :one :ref :component]
-                                      [:country/name :one :string :id]
-                                      [:country/region :one :string]]
-                            {:db/id        "datomic.tx"
-                             :db/txInstant #inst"1999"}))
-
+    @(d/transact conn schema)
     @(d/transact conn [{:m/id      "id-1"
                         :m/address {:addr/country
                                     {:country/name   "Norway"
@@ -45,8 +40,6 @@
                                      :country/region "Europe"}}}
                        {:db/id        "datomic.tx"
                         :db/txInstant #inst"2001"}])
-
-    @(d/transact conn #d/schema[[:db/txInstant2 :one :instant]])
     conn))
 
 (deftest rewrite-hist-test
@@ -67,13 +60,8 @@
              [:db/add [:tempid "5"] :country/region "Europe"]
              [:db/add "6" :addr/country [:tempid "5"]]]]
            txes))
-    @(d/transact conn [[:db.fn/retractEntity [:m/id "id-1"]]])
-    (try
-      (timbre/with-merged-config {:min-level :fatal}
-                                 (impl/pull-flat-history conn [:m/id "id-1"]))
-      (assert false "should not get here")
-      (catch Exception e
-        (is (= "Could not find lookup ref" (ex-message e)))))
-    (impl/apply-txes! conn txes)
-    (let [fh2 (impl/pull-flat-history-simple conn [:m/id "id-1"])]
-      (is (= fh2 fh)))))
+    (let [conn2 (u/empty-conn)]
+      @(d/transact conn2 schema)
+      (impl/apply-txes! conn2 txes)
+      (let [fh2 (impl/pull-flat-history-simple conn2 [:m/id "id-1"])]
+        (is (= fh2 fh))))))
