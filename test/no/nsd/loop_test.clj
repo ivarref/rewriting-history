e745486a9e257f84bdb85778bc73c57a89afa364
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-05 13:17:56 +0100

    Remove tx! usage

--- test/no/nsd/loop_test.clj
+++ test/no/nsd/loop_test.clj
@@ -7,17 +7,15 @@
 
 (deftest loops-supported
   (testing "Verify that a simple loop reference works"
-    (let [conn (u/empty-conn)
-          tx! (u/tx-fn! conn)]
+    (let [conn (u/empty-conn)]
+      @(d/transact conn #d/schema[[:m/id :one :string :id]
+                                  [:m/ref :one :ref]])
 
-      (tx! #d/schema[[:m/id :one :string :id]
-                     [:m/ref :one :ref]])
+      @(d/transact conn [{:db/id "self"
+                          :m/id  "id"
+                          :m/ref "self"}])
 
-      (tx! [{:db/id "self"
-             :m/id  "id"
-             :m/ref "self"}])
-
-      (tx! #d/schema[[:db/txInstant2 :one :instant]])
+      @(d/transact conn #d/schema[[:db/txInstant2 :one :instant]])
 
       (is (= (rh/pull-flat-history conn [:m/id "id"])
              [[1 :db/txInstant2 #inst "1972" 1 true]
@@ -25,22 +23,19 @@
               [2 :m/ref 2 1 true]]))))
 
   (testing "Verify that a nested loop reference works"
-    (let [conn (u/empty-conn)
-          tx! (u/tx-fn! conn)]
+    (let [conn (u/empty-conn)]
+      @(d/transact conn #d/schema[[:m/id :one :string :id]
+                                  [:m/comp :one :ref :component]
+                                  [:m/info :one :string]
+                                  [:m/ref :one :ref]])
 
+      @(d/transact conn [{:db/id  "self"
+                          :m/id   "id"
+                          :m/comp {:db/id  "comp"
+                                   :m/info "asdf"
+                                   :m/ref  "self"}}])
 
-      (tx! #d/schema[[:m/id :one :string :id]
-                     [:m/comp :one :ref :component]
-                     [:m/info :one :string]
-                     [:m/ref :one :ref]])
-
-      (tx! [{:db/id  "self"
-             :m/id   "id"
-             :m/comp {:db/id  "comp"
-                      :m/info "asdf"
-                      :m/ref  "self"}}])
-
-      (tx! #d/schema[[:db/txInstant2 :one :instant]])
+      @(d/transact conn #d/schema[[:db/txInstant2 :one :instant]])
 
       (is (= (rh/pull-flat-history conn [:m/id "id"])
              [[1 :db/txInstant2 #inst "1972-01-01T00:00:00.000-00:00" 1 true]
