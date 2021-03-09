2fc999a6c00bdccd07e973e551f05ab1ea88df9f
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-09 15:30:07 +0100

    Start write set intersection function WIP

--- src/no/nsd/rewriting_history/db_set_intersection_fn.clj
+++ src/no/nsd/rewriting_history/db_set_intersection_fn.clj
@@ -21,19 +21,30 @@
                          lookup-ref-or-eid)]
     [[:db/add resolved-e :db/doc doc]]))
 
-(def schema
+(def datomic-fn-def
   (clojure.edn/read-string
     {:readers {'db/id  datomic.db/id-literal
                'db/fn  datomic.function/construct
                'base64 datomic.codec/base-64-literal}}
     "{:db/ident :set/intersection\n :db/fn #db/fn \n{:lang \"clojure\", :params [db lookup-ref-or-eid doc], :requires [[datomic.api :as d] [clojure.string :as str]], :code (let [resolved-e (cond (string? lookup-ref-or-eid) lookup-ref-or-eid (vector? lookup-ref-or-eid) (let [[id v] lookup-ref-or-eid] (d/q (quote [:find ?e . :in $ ?id ?v :where [?e ?id ?v]]) db id v)) :else lookup-ref-or-eid)] [[:db/add resolved-e :db/doc doc]])}\n}"))
 
+
+(comment
+  (let [uri "datomic:mem://pet-store"
+        _ (d/delete-database uri)
+        _ (d/create-database uri)
+        conn (d/connect uri)]
+    (require '[datomic-schema.core])
+    @(d/transact conn [(generate-function false)])
+    conn))
+
+
 (comment
   (do
     (require '[rewrite-clj.zip :as z])
     (require '[clojure.edn :as edn])
     (require '[rewrite-clj.node :as n])
-    (defn generate-function []
+    (defn generate-function [write-to-file]
       (let [reqs (-> (z/of-file *file*)
                      (z/find-value z/next 'ns)
                      (z/find-value z/next :require)
@@ -66,18 +77,19 @@
                                   :code     code})
                          "\n"
                          "}")]
-        (spit *file* (-> (z/of-file *file*)
-                         (z/find-value z/next 'clojure.edn/read-string)
-                         (z/right)
-                         (z/right)
-                         (z/replace out-str)
-                         (z/root)
-                         (n/string)))
+        (when write-to-file
+          (spit *file* (-> (z/of-file *file*)
+                           (z/find-value z/next 'clojure.edn/read-string)
+                           (z/right)
+                           (z/right)
+                           (z/replace out-str)
+                           (z/root)
+                           (n/string))))
         (clojure.edn/read-string
           {:readers {'db/id  datomic.db/id-literal
                      'db/fn  datomic.function/construct
                      'base64 datomic.codec/base-64-literal}}
           out-str)))
-    (generate-function)))
+    (generate-function true)))
 
 
