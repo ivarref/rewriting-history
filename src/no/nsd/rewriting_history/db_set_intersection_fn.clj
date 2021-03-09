59516972702f9df9d83cb5d4093cd77b5c3136db
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-09 16:00:15 +0100

    Start write set intersection function WIP ...

--- src/no/nsd/rewriting_history/db_set_intersection_fn.clj
+++ src/no/nsd/rewriting_history/db_set_intersection_fn.clj
@@ -1,11 +1,18 @@
 (ns no.nsd.rewriting-history.db-set-intersection-fn
   (:require [datomic.api :as d]
             [clojure.string :as str]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint]
+            [clojure.set :as set]))
 
 (defn set--intersection
-  [db lookup-ref-or-eid a s]
-  (let [e (cond (string? lookup-ref-or-eid)
+  [db lookup-ref-or-eid a new-set]
+  (let [is-ref? (= :db.type/ref (d/q '[:find ?type .
+                                       :in $ ?attr
+                                       :where
+                                       [?attr :db/valueType ?t]
+                                       [?t :db/ident ?type]]
+                                     db a))
+        e (cond (string? lookup-ref-or-eid)
                 lookup-ref-or-eid
 
                 (vector? lookup-ref-or-eid)
@@ -20,11 +27,38 @@
 
                 :else
                 lookup-ref-or-eid)]
-    (if (string? e)
-      (let [retval (mapv (fn [set-entry] {:db/id e a set-entry}) s)]
-        (pprint/pprint retval)
+    (cond
+      (string? e)
+      (let [retval (mapv (fn [set-entry] {:db/id e a set-entry}) new-set)]
         retval)
-      nil #_[[:db/add e :db/doc doc]])))
+
+      (false? is-ref?)
+      (do
+        (let [curr-set (into #{} (d/q '[:find [?v ...]
+                                        :in $ ?e ?a
+                                        :where
+                                        [?e ?a ?v]]
+                                       db e a))
+              to-remove (set/difference curr-set new-set)
+              to-add (set/difference new-set
+                                     (set/intersection curr-set new-set))
+              tx (into []
+                   (mapv (fn [rm] [:db/retract e a rm]) to-remove)
+                   (mapv (fn [add] [:db/add e a add]) to-add))]
+          (println tx)
+          (println "new-tx:\n" (with-out-str (pprint/pprint tx)))
+          nil)
+        nil)
+
+      is-ref?
+      nil
+      #_(let [existing-set (d/q '[:find ?v
+                                  :in $ ?e ?a
+                                  :where
+                                  [?e ?a ?v]]
+                                db e a)]
+          (println "existing-set:\n" (with-out-str (pprint/pprint existing-set)))
+          nil))))
 
 (comment
   (let [uri "datomic:mem://pet-store"
@@ -35,15 +69,17 @@
     @(d/transact conn [(generate-function false)])
     @(d/transact conn #d/schema[[:m/id :one :string :id]
                                 [:m/desc :one :string]
-                                [:m/info :many :ref :component]
+                                [:m/info :many :string]
                                 [:c/a :one :string]])
 
     (let [{:keys [tempids]} @(d/transact conn [{:db/id  "id"
                                                 :m/id   "id"
                                                 :m/desc "description"}
-                                               [:set/intersection "id" :m/info #{{:c/a "a"}
-                                                                                 {:c/a "b"}}]])]
-      tempids)))
+                                               [:set/intersection "id" :m/info #{"a" "b"}]])
+          a-id (get tempids "a")]
+      @(d/transact conn [[:set/intersection [:m/id "id"] :m/info #{"b" "c"}]])
+      (->> (d/pull (d/db conn) '[:*] [:m/id "id"])
+           :m/info))))
 
 (def datomic-fn-def
   (clojure.edn/read-string
