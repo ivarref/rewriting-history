d600139830ef9af8836c98a50c82941f78e0d98e
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-10 12:28:11 +0100

    Support letfn, imports and more in datomic function generation

--- src/no/nsd/rewriting_history/db_set_intersection_fn.clj
+++ src/no/nsd/rewriting_history/db_set_intersection_fn.clj
@@ -1,78 +1,94 @@
 (ns no.nsd.rewriting-history.db-set-intersection-fn
   (:require [datomic.api :as d]
             [clojure.set :as set]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint])
+  (:import (java.util UUID)))
 
-(defn set--intersection
-  [db lookup-ref-or-eid a new-set]
-  (let [is-ref? (= :db.type/ref (d/q '[:find ?type .
-                                       :in $ ?attr
-                                       :where
-                                       [?attr :db/valueType ?t]
-                                       [?t :db/ident ?type]]
-                                     db a))
-        e (cond (string? lookup-ref-or-eid)
-                lookup-ref-or-eid
+(defn find-upsert-id [db m]
+  (assert (map? m))
+  (reduce-kv (fn [o k v])
+             nil
+             m))
 
-                (vector? lookup-ref-or-eid)
-                (let [[id v] lookup-ref-or-eid]
-                  (d/q '[:find ?e .
-                         :in $ ?id ?v
-                         :where
-                         [?e ?id ?v]]
-                       db
-                       id
-                       v))
+(defn find-upsert-id-2 [db m]
+  (reduce-kv (fn [o k v])
+             nil
+             m))
 
-                :else
-                lookup-ref-or-eid)]
-    (cond
-      (string? e)
-      (let [retval (mapv (fn [set-entry]
-                           {:db/id e a
-                                   (if (map? set-entry)
-                                     (update set-entry :db/id
-                                             (fn [v] (or v (str "a" (java.util.UUID/randomUUID)))))
-                                     set-entry)})
-                         new-set)]
-        retval)
+(defn set-intersection
+  [db m]
+  (assert (map? m) "expected m to be map")
+  (assert (map? m) "expected m to be map2")
+  (UUID/randomUUID)
+  nil
+  #_(let [is-ref? (= :db.type/ref (d/q '[:find ?type .
+                                         :in $ ?attr
+                                         :where
+                                         [?attr :db/valueType ?t]
+                                         [?t :db/ident ?type]]
+                                       db a))
+          e (cond (string? lookup-ref-or-eid)
+                  lookup-ref-or-eid
 
-      (false? is-ref?)
-      (let [curr-set (into #{} (d/q '[:find [?v ...]
-                                      :in $ ?e ?a
-                                      :where
-                                      [?e ?a ?v]]
-                                    db e a))
-            to-remove (set/difference curr-set new-set)
-            to-add (set/difference new-set (set/intersection curr-set new-set))
-            tx (reduce
-                 into
-                 []
-                 [(mapv (fn [rm] [:db/retract e a rm]) to-remove)
-                  (mapv (fn [add] [:db/add e a add]) to-add)])]
-        tx)
+                  (vector? lookup-ref-or-eid)
+                  (let [[id v] lookup-ref-or-eid]
+                    (d/q '[:find ?e .
+                           :in $ ?id ?v
+                           :where
+                           [?e ?id ?v]]
+                         db
+                         id
+                         v))
 
-      :else
-      (let [curr-set (into #{} (d/q '[:find [(pull ?v [*]) ...]
-                                      :in $ ?e ?a
-                                      :where
-                                      [?e ?a ?v]]
-                                    db e a))
-            curr-set-without-eid (into #{} (mapv (fn [ent]
-                                                   (with-meta
-                                                     (dissoc ent :db/id)
-                                                     {:db/id (:db/id ent)}))
-                                                 curr-set))
-            to-remove (->> (set/difference curr-set-without-eid new-set)
-                           (mapv (fn [e] (:db/id (meta e))))
-                           (into #{}))
-            to-add (set/difference new-set (set/intersection curr-set-without-eid new-set))
-            tx (reduce
-                 into
-                 []
-                 [(mapv (fn [rm] [:db/retract e a rm]) to-remove)
-                  (mapv (fn [add] {:db/id e a (update add :db/id (fn [v] (or v (str "a" (java.util.UUID/randomUUID)))))}) to-add)])]
-        tx))))
+                  :else
+                  lookup-ref-or-eid)]
+      (cond
+        (string? e)
+        (let [retval (mapv (fn [set-entry]
+                             {:db/id e a
+                                     (if (map? set-entry)
+                                       (update set-entry :db/id
+                                               (fn [v] (or v (str "a" (java.util.UUID/randomUUID)))))
+                                       set-entry)})
+                           new-set)]
+          retval)
+
+        (false? is-ref?)
+        (let [curr-set (into #{} (d/q '[:find [?v ...]
+                                        :in $ ?e ?a
+                                        :where
+                                        [?e ?a ?v]]
+                                      db e a))
+              to-remove (set/difference curr-set new-set)
+              to-add (set/difference new-set (set/intersection curr-set new-set))
+              tx (reduce
+                   into
+                   []
+                   [(mapv (fn [rm] [:db/retract e a rm]) to-remove)
+                    (mapv (fn [add] [:db/add e a add]) to-add)])]
+          tx)
+
+        :else
+        (let [curr-set (into #{} (d/q '[:find [(pull ?v [*]) ...]
+                                        :in $ ?e ?a
+                                        :where
+                                        [?e ?a ?v]]
+                                      db e a))
+              curr-set-without-eid (into #{} (mapv (fn [ent]
+                                                     (with-meta
+                                                       (dissoc ent :db/id)
+                                                       {:db/id (:db/id ent)}))
+                                                   curr-set))
+              to-remove (->> (set/difference curr-set-without-eid new-set)
+                             (mapv (fn [e] (:db/id (meta e))))
+                             (into #{}))
+              to-add (set/difference new-set (set/intersection curr-set-without-eid new-set))
+              tx (reduce
+                   into
+                   []
+                   [(mapv (fn [rm] [:db/retract e a rm]) to-remove)
+                    (mapv (fn [add] {:db/id e a (update add :db/id (fn [v] (or v (str "a" (java.util.UUID/randomUUID)))))}) to-add)])]
+          tx))))
 
 (def datomic-fn-def
   (clojure.edn/read-string
--- test/no/nsd/db_set_intersection_fn_test.clj
+++ test/no/nsd/db_set_intersection_fn_test.clj
@@ -7,138 +7,184 @@
             [clojure.string :as str]
             [datomic-schema.core]
             [datomic.api :as d]
-            [no.nsd.utils :as u]))
+            [no.nsd.utils :as u]
+            [clojure.tools.logging :as log]
+            [clojure.pprint :as pprint]))
 
-(defn generate-function [write-to-file]
-  (let [fil (str "src/no/nsd/rewriting_history/db_set_intersection_fn.clj")
+(defn fqn->fil [fqn]
+  (str
+    "src/"
+    (-> fqn
+        (str)
+        (str/split #"/")
+        (first)
+        (str/replace "." "/")
+        (str/replace "-" "_")
+        (str ".clj"))))
+
+(defn fqn->fn [fqn]
+  (-> fqn
+      (str)
+      (str/split #"/")
+      (last)
+      (symbol)))
+
+(defn find-fns [start-pos]
+  (loop [pos start-pos
+         fns []]
+    (if-let [next-fn (some-> pos
+                             (z/find-value z/next 'defn)
+                             (z/up))]
+      (recur (z/right next-fn)
+             (conj fns (z/sexpr next-fn))),
+      fns)))
+
+
+(defn generate-function [fqn db-name write-to-file]
+  (let [fil (fqn->fil fqn)
         reqs (-> (z/of-file fil)
                  (z/find-value z/next 'ns)
                  (z/find-value z/next :require)
                  (z/up)
                  (z/child-sexprs))
-        ident (-> (z/of-file fil)
-                  (z/find-value z/next 'defn)
-                  (z/right)
-                  (z/sexpr)
-                  (str)
-                  (str/replace "--" "/")
-                  (keyword))
-        params (-> (z/of-file fil)
-                   (z/find-value z/next 'defn)
-                   (z/right)
-                   (z/right)
-                   (z/sexpr))
-        code (-> (z/of-file fil)
-                 (z/find-value z/next 'defn)
-                 (z/right)
-                 (z/right)
-                 (z/right)
-                 (z/sexpr))
-        out-str (str "{:db/ident " ident "\n"
+        impos (-> (z/of-file fil)
+                  (z/find-value z/next 'ns)
+                  (z/find-value z/next :import)
+                  (z/up)
+                  (z/child-sexprs))
+        ident (fqn->fn fqn)
+        fndef (-> (z/of-file fil)
+                  (z/find-value z/next ident)
+                  (z/up)
+                  (z/sexpr))
+        other-defns (->> (find-fns (z/of-file fil))
+                         (remove #(= ident (second %)))
+                         (mapv (fn [[_defn id args & body]]
+                                 (list id args (apply list 'do body)))))
+        params (some-> (z/of-file fil)
+                       (z/find-value z/next ident)
+                       (z/right)
+                       (z/sexpr))
+        code (some-> (z/of-file fil)
+                     (z/find-value z/next ident)
+                     (z/remove)
+                     (z/remove)
+                     (z/down)
+                     (z/remove)
+                     (z/edit (fn [n]
+                               (list 'letfn other-defns
+                                     (apply list 'do n))))
+                     (z/sexpr))
+        db-fn {:lang     "clojure"
+               :requires (vec (drop 1 reqs))
+               :imports (vec (drop 1 impos))
+               :params   params
+               :code     code}
+        out-str (str "{:db/ident " db-name "\n"
                      " :db/fn #db/fn \n"
-                     (pr-str {:lang     "clojure"
-                              :params   params
-                              :requires (vec (drop 1 reqs))
-                              :code     code})
+                     (pr-str db-fn)
                      "\n"
                      "}")]
-    (when write-to-file
-      (spit fil (-> (z/of-file fil)
-                    (z/find-value z/next 'clojure.edn/read-string)
-                    (z/right)
-                    (z/right)
-                    (z/replace out-str)
-                    (z/root)
-                    (n/string))))
     (clojure.edn/read-string
       {:readers {'db/id  datomic.db/id-literal
                  'db/fn  datomic.function/construct
                  'base64 datomic.codec/base-64-literal}}
       out-str)))
 
-(defn get-curr-set [conn]
-  (->> (d/pull (d/db conn) '[:*] [:m/id "id"])
-       :m/set
-       (mapv (fn [v] (if (and
-                           (map? v)
-                           (= [:db/id] (vec (keys v))))
-                       (d/pull (d/db conn) '[:*] (:db/id v))
-                       v)))
-       (mapv (fn [v] (if (map? v)
-                       (dissoc v :db/id)
-                       v)))
-       (into #{})))
+(comment
+  (generate-function 'no.nsd.rewriting-history.db-set-intersection-fn/set-intersection false))
+#_(defn get-curr-set [conn]
+    (->> (d/pull (d/db conn) '[:*] [:m/id "id"])
+         :m/set
+         (mapv (fn [v] (if (and
+                             (map? v)
+                             (= [:db/id] (vec (keys v))))
+                         (d/pull (d/db conn) '[:*] (:db/id v))
+                         v)))
+         (mapv (fn [v] (if (map? v)
+                         (dissoc v :db/id)
+                         v)))
+         (into #{})))
 
 (deftest verify-primitives-work
-  (let [schema (reduce into []
-                       [[(generate-function false)]
-                        #d/schema[[:m/id :one :string :id]
-                                  [:m/set :many :string]]])
-        conn (u/empty-conn schema)]
-    @(d/transact conn [{:db/id "id" :m/id "id"}
-                       [:set/intersection "id" :m/set #{"a" "b"}]])
-    (is (= #{"a" "b"} (get-curr-set conn)))
+  (log/info "code is:\n"
+            (with-out-str
+              (pprint/pprint
+                (generate-function
+                  'no.nsd.rewriting-history.db-set-intersection-fn/set-intersection
+                  :set/intersection
+                  false))))
+  #_(let [schema (reduce into []
+                         [[(generate-function false)]
+                          #d/schema[[:m/id :one :string :id]
+                                    [:m/set :many :string]]])
+          conn (u/empty-conn schema)]
+      @(d/transact conn [[:set/intersection
+                          {:m/id  "id"
+                           :m/set #{"a" "b"}}]
+                         #_[:set/intersection "id"]])
+      #_(is (= #{"a" "b"} (get-curr-set conn)))))
 
-    @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{"b" "c"}]])
-    (is (= #{"b" "c"} (get-curr-set conn)))
+;@(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{"b" "c"}]])
+;(is (= #{"b" "c"} (get-curr-set conn)))
+;
+;@(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{}]])
+;(is (= #{} (get-curr-set conn)))))
 
-    @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{}]])
-    (is (= #{} (get-curr-set conn)))))
+#_(deftest verify-component-refs-work
+    (let [schema (reduce into []
+                         [[(generate-function false)]
+                          #d/schema[[:m/id :one :string :id]
+                                    [:m/set :many :ref :component]
+                                    [:c/a :one :string]]])
+          conn (u/empty-conn, schema)]
+      @(d/transact conn [{:db/id "id" :m/id "id"}
+                         [:set/intersection "id" :m/set #{{:c/a "a"} {:c/a "b"}}]])
+      (is (= #{{:c/a "a"} {:c/a "b"}} (get-curr-set conn)))
 
-(deftest verify-component-refs-work
-  (let [schema (reduce into []
-                       [[(generate-function false)]
-                        #d/schema[[:m/id :one :string :id]
-                                  [:m/set :many :ref :component]
-                                  [:c/a :one :string]]])
-        conn (u/empty-conn, schema)]
-    @(d/transact conn [{:db/id "id" :m/id "id"}
-                       [:set/intersection "id" :m/set #{{:c/a "a"} {:c/a "b"}}]])
-    (is (= #{{:c/a "a"} {:c/a "b"}} (get-curr-set conn)))
+      (let [b-eid (d/q
+                    '[:find ?e .
+                      :in $
+                      :where
+                      [?e :c/a "b"]]
+                    (d/db conn))]
+        @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{{:c/a "b"} {:c/a "c"}}]])
+        (is (= #{{:c/a "b"} {:c/a "c"}} (get-curr-set conn)))
+        (is (= b-eid
+               (d/q
+                 '[:find ?e .
+                   :in $
+                   :where
+                   [?e :c/a "b"]]
+                 (d/db conn)))))))
 
-    (let [b-eid (d/q
-                  '[:find ?e .
-                    :in $
-                    :where
-                    [?e :c/a "b"]]
-                  (d/db conn))]
-      @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{{:c/a "b"} {:c/a "c"}}]])
-      (is (= #{{:c/a "b"} {:c/a "c"}} (get-curr-set conn)))
-      (is (= b-eid
-             (d/q
-               '[:find ?e .
-                 :in $
-                 :where
-                 [?e :c/a "b"]]
-               (d/db conn)))))))
+#_(deftest verify-refs-work
+    (let [schema (reduce into []
+                         [[(generate-function false)]
+                          #d/schema[[:m/id :one :string :id]
+                                    [:m/set :many :ref]
+                                    [:c/a :one :string]]])
+          conn (u/empty-conn, schema)]
+      @(d/transact conn [{:db/id "id" :m/id "id"}
+                         [:set/intersection "id" :m/set #{{:c/a "a"} {:c/a "b"}}]])
+      (is (= #{{:c/a "a"} {:c/a "b"}} (get-curr-set conn)))
 
-(deftest verify-refs-work
-  (let [schema (reduce into []
-                       [[(generate-function false)]
-                        #d/schema[[:m/id :one :string :id]
-                                  [:m/set :many :ref]
-                                  [:c/a :one :string]]])
-        conn (u/empty-conn, schema)]
-    @(d/transact conn [{:db/id "id" :m/id "id"}
-                       [:set/intersection "id" :m/set #{{:c/a "a"} {:c/a "b"}}]])
-    (is (= #{{:c/a "a"} {:c/a "b"}} (get-curr-set conn)))
+      (let [b-eid (d/q
+                    '[:find ?e .
+                      :in $
+                      :where
+                      [?e :c/a "b"]]
+                    (d/db conn))]
+        @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{{:c/a "b"} {:c/a "c"}}]])
+        (is (= #{{:c/a "b"} {:c/a "c"}} (get-curr-set conn)))
+        (is (= b-eid
+               (d/q
+                 '[:find ?e .
+                   :in $
+                   :where
+                   [?e :c/a "b"]]
+                 (d/db conn)))))))
 
-    (let [b-eid (d/q
-                  '[:find ?e .
-                    :in $
-                    :where
-                    [?e :c/a "b"]]
-                  (d/db conn))]
-      @(d/transact conn [[:set/intersection [:m/id "id"] :m/set #{{:c/a "b"} {:c/a "c"}}]])
-      (is (= #{{:c/a "b"} {:c/a "c"}} (get-curr-set conn)))
-      (is (= b-eid
-             (d/q
-               '[:find ?e .
-                 :in $
-                 :where
-                 [?e :c/a "b"]]
-               (d/db conn)))))))
-
-(deftest gen-fn
-  (generate-function true)
-  (is (= 1 1)))
+#_(deftest gen-fn
+    (generate-function true)
+    (is (= 1 1)))
