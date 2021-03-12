9b12819d750b7a17df6506631a647c8fea108b44
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-12 13:14:11 +0100

    Write tests for set-disj and improve sanity checks

--- src/no/nsd/rewriting_history/dbfns/set_disj.clj
+++ src/no/nsd/rewriting_history/dbfns/set_disj.clj
@@ -31,12 +31,12 @@
                                          db attr)))
                   (str "expected " attr " to be of valueType :db.type/ref"))
 
-        _ (assert (true?
-                    (= :db.cardinality/many (d/q '[:find ?c .
-                                                   :in $ ?attr
-                                                   :where
-                                                   [?attr :db/cardinality ?c]]
-                                                 db attr)))
+        _ (assert (= :db.cardinality/many (d/q '[:find ?type .
+                                                 :in $ ?attr
+                                                 :where
+                                                 [?attr :db/cardinality ?c]
+                                                 [?c :db/ident ?type]]
+                                               db attr))
                   (str "expected attribute to have cardinality :db.cardinality/many"))
 
         db (if (instance? Database db) db (d/db db))
--- test/no/nsd/db_set_disj_test.clj
+++ test/no/nsd/db_set_disj_test.clj
@@ -6,6 +6,7 @@
             [datomic-schema.core]
             [datomic.api :as d]
             [clojure.tools.logging :as log]
+            [no.nsd.shorter-stacktrace]
             [clojure.string :as str]))
 
 (defn db-fn
@@ -40,14 +41,7 @@
            (log/error "got error message" emsg#))
          v#)))
 
-(deftest set-disj-tests
-  (testing "fails on non-map input"
-    (let [conn (u/empty-conn)]
-      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
-                                                  [:m/ref :many :ref :component]]))
-      (is-assert-msg "Assert failed: (map? value)"
-                     @(d/transact conn [[:set/disj [:m/id "id"] :m/ref "asdf"]]))))
-
+(deftest set-disj-should-fail-tests
   (testing "fails on not-many attribute"
     (let [conn (u/empty-conn)]
       @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
@@ -60,7 +54,56 @@
       @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
                                                   [:m/ref :many :string]]))
       (is-assert-msg "expected :m/ref to be of valueType :db.type/ref"
-                     @(d/transact conn [[:set/disj [:m/id "id"] :m/ref {:x :x}]])))))
+                     @(d/transact conn [[:set/disj [:m/id "id"] :m/ref {:x :x}]]))))
 
+  (testing "fails on non-map input"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
+                                                  [:m/ref :many :ref :component]]))
+      (is-assert-msg "Assert failed: (map? value)"
+                     @(d/transact conn [[:set/disj [:m/id "id"] :m/ref "asdf"]])))))
 
+(deftest set-disj-should-ok-tests
+  (testing "OK on missing lookup ref"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
+                                                  [:m/ref :many :ref :component]]))
+      @(d/transact conn [[:set/disj [:m/id "id"] :m/ref {:x :x}]])))
 
+  (testing "OK on empty set"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
+                                                  [:m/ref :many :ref :component]]))
+      @(d/transact conn [{:m/id "id"}])
+      @(d/transact conn [[:set/disj [:m/id "id"] :m/ref {:x :x}]])))
+
+  (testing "OK on missing element"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
+                                                  [:m/set :many :ref :component]
+                                                  [:e/id :one :string]]))
+      @(d/transact conn [{:m/id "id" :m/set #{{:e/id "a"}}}])
+      @(d/transact conn [[:set/disj [:m/id "id"] :m/set {:e/id "b"}]])
+
+      (is (= (->> (d/pull (d/db conn) [:*] [:m/id "id"])
+                  :m/set
+                  (mapv :e/id)
+                  (sort)
+                  (vec))
+             ["a"]))))
+
+  (testing "OK on found element"
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (into [(db-fn)] #d/schema[[:m/id :one :string :id]
+                                                  [:m/set :many :ref :component]
+                                                  [:e/id :one :string]]))
+      @(d/transact conn [{:m/id "id" :m/set #{{:e/id "a"} {:e/id "b"} {:e/id "c"}}}])
+
+      @(d/transact conn [[:set/disj [:m/id "id"] :m/set {:e/id "b"}]])
+
+      (is (= (->> (d/pull (d/db conn) [:*] [:m/id "id"])
+                  :m/set
+                  (mapv :e/id)
+                  (sort)
+                  (vec))
+             ["a" "c"])))))
