496cab9c85d99f2183b6387706f7357f6b7611bb
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-15 11:34:46 +0100

    Can wipe rewrite jobs

--- src/no/nsd/rewriting_history/wipe.clj
+++ src/no/nsd/rewriting_history/wipe.clj
@@ -1,13 +1,20 @@
 (ns no.nsd.rewriting-history.wipe
-  (:require [no.nsd.rewriting-history.impl :as impl])
+  (:require [no.nsd.rewriting-history.impl :as impl]
+            [datomic.api :as d]
+            [clojure.tools.logging :as log])
   (:import (datomic Connection)))
 
 (defn wipe-rewrite-job! [conn lookup-ref]
   (assert (vector? lookup-ref))
   (assert (instance? Connection conn))
-  (let [history (impl/pull-flat-history conn [:rh/lookup-ref (pr-str lookup-ref)])]
-    (->> history
-         (filter (fn [[_e a _v _t _o]] (= "rh" (namespace a))))
-         (map first)
-         (into #{}))))
-
+  (when (impl/resolve-lookup-ref conn [:rh/lookup-ref (pr-str lookup-ref)])
+    (let [history (impl/pull-flat-history conn [:rh/lookup-ref (pr-str lookup-ref)])
+          eids (->> history
+                    (filter (fn [[_e a _v _t _o]] (= "rh" (namespace a))))
+                    (map first)
+                    (into #{}))
+          tx (mapv (fn [eid] {:db/excise eid})
+                   eids)]
+      (log/debug "excising" (count tx) "entities ...")
+      (let [{:keys [db-after]} @(d/transact conn tx)]
+        @(d/sync-excise conn (d/basis-t db-after))))))
--- test/no/nsd/wipe_rewrite_job_test.clj
+++ test/no/nsd/wipe_rewrite_job_test.clj
@@ -8,6 +8,7 @@
             [no.nsd.shorter-stacktrace]
             [no.nsd.rewriting-history.impl :as impl]
             [no.nsd.log-init]
+            [no.nsd.rewriting-history.wipe :as wipe]
             [no.nsd.rewriting-history.replay-impl :as replay]))
 
 (defn db-values-set [conn]
@@ -28,4 +29,42 @@
                                   [:tx/txInstant :one :instant]]))
     @(d/transact conn [{:m/id "id" :m/info "original-data"}])
     @(d/transact conn [{:m/id "id" :m/info "bad-data"}])
-    @(d/transact conn [{:m/id "id" :m/info "good-data"}])))
+    @(d/transact conn [{:m/id "id" :m/info "good-data"}])
+
+    ; Verify that all expected data is present in the database:
+    (is (= #{"original-data" "bad-data" "good-data"} (db-values-set conn)))
+
+    (let [org-history (rh/pull-flat-history conn [:m/id "id"])]
+      ; Original history looks like this:
+      (is (= org-history
+             [[1 :tx/txInstant #inst "1972" 1 true]
+              [4 :m/id "id" 1 true]
+              [4 :m/info "original-data" 1 true]
+              [2 :tx/txInstant #inst "1973" 2 true]
+              [4 :m/info "original-data" 2 false]
+              [4 :m/info "bad-data" 2 true]
+              [3 :tx/txInstant #inst "1974" 3 true]
+              [4 :m/info "bad-data" 3 false]
+              [4 :m/info "good-data" 3 true]]))
+
+      (rh/schedule-replacement! conn [:m/id "id"] "bad-data" "corrected-data")
+      (replay/process-until-state conn [:m/id "id"] :done)
+
+      (is (= #{"original-data" "corrected-data" "good-data"} (db-values-set conn)))
+
+      (is (= (rh/pull-flat-history conn [:m/id "id"])
+             [[1 :tx/txInstant #inst "1972" 1 true]
+              [4 :m/id "id" 1 true]
+              [4 :m/info "original-data" 1 true]
+              [2 :tx/txInstant #inst "1973" 2 true]
+              [4 :m/info "original-data" 2 false]
+              [4 :m/info "corrected-data" 2 true]
+              [3 :tx/txInstant #inst "1974" 3 true]
+              [4 :m/info "corrected-data" 3 false]
+              [4 :m/info "good-data" 3 true]]))
+
+      (is (= 161 (count (rh/pull-flat-history conn [:rh/lookup-ref (pr-str [:m/id "id"])]))))
+
+      (wipe/wipe-rewrite-job! conn [:m/id "id"])
+
+      (is (zero? (count (rh/pull-flat-history conn [:rh/lookup-ref (pr-str [:m/id "id"])])))))))
