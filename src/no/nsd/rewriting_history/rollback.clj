03dec7b28d9eb9ebdf9c67233f6105013633d3ea
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-19 11:32:55 +0100

    Simplify rollback view if possible

--- src/no/nsd/rewriting_history/rollback.clj
+++ src/no/nsd/rewriting_history/rollback.clj
@@ -3,31 +3,61 @@
             [no.nsd.rewriting-history.impl :as impl]
             [no.nsd.rewriting-history.add-rewrite-job :as add-job]
             [clojure.tools.logging :as log])
-  (:import (datomic Connection)))
+  (:import (datomic Connection)
+           (java.util Date)))
 
 (defn rollback! [conn lookup-ref t]
   (assert (instance? Connection conn))
   (assert (some? (impl/resolve-lookup-ref conn [:rh/lookup-ref (pr-str lookup-ref)])))
-  (assert (pos-int? t))
-  (log/info "checking t ...")
-  (assert (= t (d/q '[:find ?t .
-                      :in $ ?lookup-ref ?t
-                      :where
-                      [?e :rh/lookup-ref ?lookup-ref]
-                      [?e :rh/state :init ?t true]]
-                    (impl/history conn)
-                    (pr-str lookup-ref)
-                    t))
-          "Expected to find t in history with :rh/state set to :init")
-  (log/info "checking t ... OK")
-  (let [new-history (impl/pull-flat-history-simple (impl/as-of conn t) lookup-ref)]
-    (log/info "adding rollback job...")
-    (add-job/add-job! conn lookup-ref :ok-rollback :pending-rollback new-history)
-    (log/info "adding rollback job... OK!")))
+  (assert (or (instance? Date t) (pos-int? t)))
+  (if (instance? Date t)
+    (do
+      (log/info "resolving t ...")
+      (rollback! conn lookup-ref
+                 (d/q '[:find ?t .
+                        :in $ ?lookup-ref ?inst
+                        :where
+                        [?e :rh/lookup-ref ?lookup-ref]
+                        [?e :rh/state :init ?t true]
+                        [?t :db/txInstant ?inst]]
+                      (impl/history conn)
+                      (pr-str lookup-ref)
+                      t)))
+    (do
+      (assert (= t (d/q '[:find ?t .
+                          :in $ ?lookup-ref ?t
+                          :where
+                          [?e :rh/lookup-ref ?lookup-ref]
+                          [?e :rh/state :init ?t true]]
+                        (impl/history conn)
+                        (pr-str lookup-ref)
+                        t))
+              "Expected to find t in history with :rh/state set to :init")
+      (log/info "checking t ... OK")
+      (let [new-history (impl/pull-flat-history-simple (impl/as-of conn t) lookup-ref)]
+        (log/info "adding rollback job...")
+        (add-job/add-job! conn lookup-ref :ok-rollback :pending-rollback new-history)
+        (log/info "adding rollback job... OK!")))))
 
+(defn available-times [conn lookup-ref]
+  (assert (vector? lookup-ref))
+  (let [db (impl/history conn)
+        times (->> (d/q '[:find ?inst ?t
+                          :in $ ?lookup-ref
+                          :where
+                          [?e :rh/lookup-ref ?lookup-ref]
+                          [?e :rh/state :init ?t true]
+                          [?t :db/txInstant ?inst]]
+                        db
+                        (pr-str lookup-ref))
+                   (map (fn [[inst t]] {inst [t]}))
+                   (reduce (partial merge-with into) {})
+                   (map (fn [[k v]] [k (vec (sort v))]))
+                   (sort)
+                   (vec))]
+    (if (every? #(= 1 (count (second %))) times)
+      (into (sorted-set) (mapv first times))
+      times)))
 
-
-
-
-
-
+(comment
+  (available-times c [:m/id "id"]))
