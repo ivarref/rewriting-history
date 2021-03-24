7911cfe58a30d1b8443c7a297c9511af428f000f
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-24 20:24:36 +0100

    Add censored stage test data ...

---
+++ test/no/nsd/stage_data_test.clj
@@ -0,0 +1,20 @@
+(ns no.nsd.stage-data-test
+  (:require [clojure.test :refer :all]
+            [no.nsd.utils :as u]
+            [datomic.api :as d]
+            [no.nsd.rewriting-history :as rh]
+            [no.nsd.schema :as schema]
+            [no.nsd.stage-data :as sd]
+            [clojure.tools.logging :as log]
+            [no.nsd.rewriting-history.replay-impl :as replay]
+            [no.nsd.rewriting-history.add-rewrite-job :as add-job]))
+
+(deftest stage-data-test
+  (let [conn (u/empty-conn-days-txtime)]
+    (rh/init-schema! conn)
+    @(d/transact conn schema/schema)
+    (let [new-hist sd/data
+          mref [:Meldeskjema/ref "be0bc167-8015-4104-af21-0727ffb3d95d"]]
+      (add-job/add-job! conn mref #{nil} :testing new-hist)
+      (replay/process-job-step! conn mref)
+      (time (replay/process-until-state conn mref :done)))))
