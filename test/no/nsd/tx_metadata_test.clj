172b1891ed6103f35716dd8cb83e33186c0c1af1
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-24 19:57:20 +0100

    Use new test style

--- test/no/nsd/tx_metadata_test.clj
+++ test/no/nsd/tx_metadata_test.clj
@@ -4,18 +4,17 @@
             [datomic.api :as d]
             [no.nsd.utils :as u]
             [no.nsd.rewriting-history.impl :as impl]
-            [no.nsd.shorter-stacktrace]
-            [no.nsd.log-init]
-            [no.nsd.spy :as sc]
+            [no.nsd.rewriting-history.tx :as tx]
             [clojure.pprint :as pprint]))
 
 (deftest tx-meta-test
   (testing "Verify that transaction metadata is included in history"
-    (let [schema #d/schema[[:m/id :one :string :id]
-                           [:m/info :one :string]
-                           [:tx/info :one :string]
-                           [:tx/txInstant :one :instant]]
-          conn (u/empty-conn schema)]
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (reduce into []
+                                [#d/schema[[:m/id :one :string :id]
+                                           [:m/info :one :string]
+                                           [:tx/info :one :string]]
+                                 impl/schema]))
       @(d/transact conn [{:m/id   "id"
                           :m/info "hello world"}
                          {:db/id        "datomic.tx"
@@ -26,25 +25,30 @@
                 [2 :m/id "id" 1 true]
                 [2 :m/info "hello world" 1 true]]
                fh))
-        (let [[tx] (impl/history->transactions conn fh)]
+        (let [[tx] (tx/generate-tx conn [:m/id "id"] fh)]
           (is (= tx
                  [[:db/add "datomic.tx" :tx/info "meta"]
-                  [:db/add "datomic.tx" :tx/txInstant #inst "1972"]
+                  [:db/add "datomic.tx" :tx/txInstant #inst "1972-01-01T00:00:00.000-00:00"]
                   [:db/add "2" :m/id "id"]
-                  [:db/add "2" :m/info "hello world"]]))
-          (let [conn (u/empty-conn schema)]
-            @(d/transact conn tx)
-            (let [fh2 (impl/pull-flat-history-simple conn [:m/id "id"])]
-              (is (= fh2 fh)))))))))
+                  [:db/add "2" :m/info "hello world"]
+                  [:set/union
+                   [:rh/lookup-ref "[:m/id \"id\"]"]
+                   :rh/tempids
+                   #{#:rh{:tempid-str "2", :tempid-ref "2"} #:rh{:tempid-str "1", :tempid-ref "datomic.tx"}}]
+                  [:db/cas [:rh/lookup-ref "[:m/id \"id\"]"] :rh/state :rewrite-history :verify]]))
+          (u/rewrite-noop! conn [:m/id "id"])
+          (is (= fh (impl/pull-flat-history-simple conn [:m/id "id"]))))))))
 
 (deftest tx-meta-ref-test
   (testing "Verify that :ent/ref 'datomic.tx' works"
-    (let [schema #d/schema[[:m/id :one :string :id]
-                           [:m/info :one :string]
-                           [:m/ref :one :ref]
-                           [:tx/info :one :string]
-                           [:tx/txInstant :one :instant]]
-          conn (u/empty-conn schema)]
+    (let [conn (u/empty-conn)]
+      @(d/transact conn (reduce into []
+                                [#d/schema[[:m/id :one :string :id]
+                                           [:m/info :one :string]
+                                           [:m/ref :one :ref]
+                                           [:tx/info :one :string]
+                                           [:tx/txInstant :one :instant]]
+                                 impl/schema]))
       @(d/transact conn [{:m/id   "id"
                           :m/info "hello world"
                           :m/ref  "datomic.tx"}
@@ -59,13 +63,17 @@
                 [2 :m/info "hello world" 1 true]
                 [2 :m/ref 1 1 true]]
                fh))
-        (let [[tx] (impl/history->transactions conn fh)]
+        (let [[tx] (tx/generate-tx conn [:m/id "id"] fh)]
           (is (= tx
                  [[:db/add "datomic.tx" :tx/info "meta"]
-                  [:db/add "datomic.tx" :tx/txInstant #inst "1972"]
+                  [:db/add "datomic.tx" :tx/txInstant #inst "1972-01-01T00:00:00.000-00:00"]
                   [:db/add "2" :m/id "id"]
                   [:db/add "2" :m/info "hello world"]
-                  [:db/add "2" :m/ref "datomic.tx"]]))
-          (let [conn (u/empty-conn schema)]
-            @(d/transact conn tx)
-            (is (= fh (impl/pull-flat-history-simple conn [:m/id "id"])))))))))
+                  [:db/add "2" :m/ref "datomic.tx"]
+                  [:set/union
+                   [:rh/lookup-ref "[:m/id \"id\"]"]
+                   :rh/tempids
+                   #{#:rh{:tempid-str "2", :tempid-ref "2"} #:rh{:tempid-str "1", :tempid-ref "datomic.tx"}}]
+                  [:db/cas [:rh/lookup-ref "[:m/id \"id\"]"] :rh/state :rewrite-history :verify]]))
+          (u/rewrite-noop! conn [:m/id "id"])
+          (is (= fh (impl/pull-flat-history-simple conn [:m/id "id"]))))))))
