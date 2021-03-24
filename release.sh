14ee07c044d18401ee92942067e6842f02dd4add
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-24 13:07:20 +0100

    Fix repos and simplify release script

--- release.sh
+++ release.sh
@@ -9,22 +9,17 @@ clojure -M:jar
 COMMIT_COUNT="$(git rev-list --count HEAD)"
 let "NEXT_PATCH=COMMIT_COUNT+1"
 
-sed -i 's|^  <version>\(.*\..*\.\).*</version>$|  <version>\1'$NEXT_PATCH'</version>|' pom.xml
-
-RAW_VERSION="$(grep "^  <version>" pom.xml | sed 's|^.*<version>\(.*\)</version>.*$|\1|')"
-
-NEW_VERSION="v$RAW_VERSION"
-echo "Releasing >$NEW_VERSION< ..."
-
-sed -i 's|^    <tag>.*$|    <tag>'$NEW_VERSION'</tag>|' pom.xml
+clojure -X:release ivarref.pom-patch/clojars-repo-only!
+VERSION=$(clojure -X:release ivarref.pom-patch/set-patch-version! :patch "$NEXT_PATCH")
+clojure -X:release ivarref.pom-patch/update-tag!
 
 git add pom.xml
-git commit -m "Release $NEW_VERSION"
-git tag -a $NEW_VERSION -m "Release $NEW_VERSION"
+git commit -m "Release $VERSION"
+git tag -a v$VERSION -m "Release v$VERSION"
 git push --follow-tags
 
 clojure -M:deploy
 
-echo "Released $RAW_VERSION"
+echo "Released $VERSION"
 
 rm *.pom.asc
