9ded1fcdefef9822b1deb6dca2e26b1bec65d7d3
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-03-24 21:46:26 +0100

    Speed up rewrite history performance

--- test/no/nsd/stage_data_test.clj
+++ test/no/nsd/stage_data_test.clj
@@ -7,7 +7,8 @@
             [no.nsd.stage-data :as sd]
             [clojure.tools.logging :as log]
             [no.nsd.rewriting-history.replay-impl :as replay]
-            [no.nsd.rewriting-history.add-rewrite-job :as add-job]))
+            [no.nsd.rewriting-history.add-rewrite-job :as add-job]
+            [no.nsd.rewriting-history.impl :as impl]))
 
 (deftest stage-data-test
   (let [conn (u/empty-conn-days-txtime)]
@@ -17,4 +18,7 @@
           mref [:Meldeskjema/ref "be0bc167-8015-4104-af21-0727ffb3d95d"]]
       (add-job/add-job! conn mref #{nil} :testing new-hist)
       (replay/process-job-step! conn mref)
-      (replay/process-until-state conn mref :done))))
+      (replay/process-until-state conn mref :done)
+      (is (= new-hist
+             (impl/pull-flat-history-simple conn mref))))))
+
