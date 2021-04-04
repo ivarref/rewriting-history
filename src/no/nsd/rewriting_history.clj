0548545cb463646dedee6e82cd84a0aeaed63b9b
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-04-04 19:48:19 +0200

    Update rewriting_history.clj

--- src/no/nsd/rewriting_history.clj
+++ src/no/nsd/rewriting_history.clj
@@ -14,28 +14,32 @@
 (defn schedule-replacement!
   "Schedule a replacement of ^String match with ^String replacement
   for lookup-ref.
-  This will create or update a rewrite-job that will need to be triggered
-  at a later stage using process-scheduled!"
+  Creates or updates a rewrite-job."
   [conn lookup-ref match replacement]
   (schedule/schedule-replacement! conn lookup-ref match replacement))
 
 (defn cancel-replacement! [conn lookup-ref match replacement]
-  "Cancel a scheduled replacement for lookup-ref."
+  "Cancel a scheduled replacement for lookup-ref. Updates an existing rewrite-job."
   (schedule/cancel-replacement! conn lookup-ref match replacement))
 
 (defn pending-replacements [conn lookup-ref]
+  "Get pending replacements for lookup-ref."
   (schedule/pending-replacements (d/db conn) lookup-ref))
 
 (defn rewrite-scheduled! [conn]
+  "Process all rewrite-jobs that are scheduled." 
   (replay/process-state! conn :scheduled))
 
 (defn excise-old-rewrite-jobs! [conn older-than-days]
+  "Excise all rewrite-job metadata."
   (wipe/wipe-old-rewrite-jobs! conn (Date.) older-than-days))
 
 (defn available-rollback-times [conn lookup-ref]
+  "Get available rollback times for a lookup ref. Returns a set of java.util.Dates."
   (rollback/available-times conn lookup-ref))
 
-(defn rollback! [conn lookup-ref t]
+(defn rollback! [conn lookup-ref ^Date t]
+  "Rollback lookup-ref to a previous ^java.util.Date t."
   (rollback/rollback! conn lookup-ref t))
 
 ; convenience methods
@@ -47,4 +51,5 @@
   (impl/pull-flat-history-simple db lookup-ref))
 
 (defn get-org-history [db lookup-ref]
+  "Get the original flat history of lookup-ref as it was before rewriting started."
   (impl/get-org-history db lookup-ref))
