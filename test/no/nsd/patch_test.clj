3213f3d3ad342933ed675ac00a890a1182fa1763
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-05-18 13:56:31 +0200

    Fix cancel patch test

--- test/no/nsd/patch_test.clj
+++ test/no/nsd/patch_test.clj
@@ -25,50 +25,50 @@
                        (rh/assoc-lookup-ref [:fil/id u/uuid-1]
                                             :fil/id u/uuid-0
                                             :fil/name "deleted.txt"))]
-      (is (= [[3 :m/files 4 1 true]
-              [3 :m/id "id" 1 true]
-              [4 :fil/id u/uuid-1 1 true]
-              [4 :fil/name "secret.txt" 1 true]
-              [4 :fil/id u/uuid-1 2 false]
-              [4 :fil/id u/uuid-2 2 true]
-              [4 :fil/name "secret.txt" 2 false]
-              [4 :fil/name "new.txt" 2 true]]
+      (is (= [[1 :m/files 2 -1 true]
+              [1 :m/id "id" -1 true]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000001" -1 true]
+              [2 :fil/name "secret.txt" -1 true]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000001" -2 false]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000002" -2 true]
+              [2 :fil/name "secret.txt" -2 false]
+              [2 :fil/name "new.txt" -2 true]]
              (u/ignore-txInstant hist)))
-      (is (= [[3 :m/files 4 1 true]
-              [3 :m/id "id" 1 true]
-              [4 :fil/id u/uuid-0 1 true]
-              [4 :fil/name "deleted.txt" 1 true]
-              [4 :fil/id u/uuid-0 2 false]
-              [4 :fil/id u/uuid-2 2 true]
-              [4 :fil/name "deleted.txt" 2 false]
-              [4 :fil/name "new.txt" 2 true]]
+      (is (= [[1 :m/files 2 -1 true]
+              [1 :m/id "id" -1 true]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000000" -1 true]
+              [2 :fil/name "deleted.txt" -1 true]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000000" -2 false]
+              [2 :fil/id #uuid "00000000-0000-0000-0000-000000000002" -2 true]
+              [2 :fil/name "deleted.txt" -2 false]
+              [2 :fil/name "new.txt" -2 true]]
              (u/ignore-txInstant new-hist)))
       (is (= {:add
-                      [{:e "4", :a ":fil/id", :v "uid:0", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:0", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:0", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:0", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :remove
-                      [{:e "4", :a ":fil/id", :v "uid:1", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:1", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:1", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:1", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :status "Scheduled patch",
-              :tags   ["[:delete-file uid:1]"]}
+              :tags ["[:delete-file uid:1]"]}
              (u/abbr (rh/schedule-patch! conn [:m/id "id"] [:delete-file u/uuid-1] hist new-hist))))
       (is (= {:add [], :remove [], :status "Cancelled patch", :tags []}
              (rh/cancel-patch! conn [:m/id "id"] [:delete-file u/uuid-1] hist new-hist)))
       (is (= nil (rh/get-job-state conn [:m/id "id"])))
       (is (= {:add
-                      [{:e "4", :a ":fil/id", :v "uid:0", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:0", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:0", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:0", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :remove
-                      [{:e "4", :a ":fil/id", :v "uid:1", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:1", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:1", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:1", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :status "Scheduled patch",
               :tags ["[:delete-file uid:1]"]}
              (u/abbr (rh/schedule-patch! conn [:m/id "id"] [:delete-file u/uuid-1] hist new-hist))))
@@ -77,15 +77,15 @@
              (u/abbr (rh/cancel-patch-tag! conn [:delete-file u/uuid-1]))))
 
       (is (= {:add
-                      [{:e "4", :a ":fil/id", :v "uid:0", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:0", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:0", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:0", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :remove
-                      [{:e "4", :a ":fil/id", :v "uid:1", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:1", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:1", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:1", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :status "Scheduled patch",
               :tags ["[:delete-file uid:1]"]}
              (u/abbr (rh/schedule-patch! conn [:m/id "id"] [:delete-file u/uuid-1] hist new-hist))))
@@ -103,22 +103,22 @@
                                                :fil/id u/uuid-4
                                                :fil/name "deleted2.txt"))
       (is (= {:add
-                      [{:e "3", :a ":fil/id", :v "uid:4", :t "1", :o "true", :ref "[:m/id i2]"}
-                       {:e "3", :a ":fil/name", :v "deleted2.txt", :t "1", :o "true", :ref "[:m/id i2]"}
-                       {:e "4", :a ":fil/id", :v "uid:0", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:0", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "deleted.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:4", :t "-1", :o "true", :ref "[:m/id i2]"}
+                       {:e "2", :a ":fil/name", :v "deleted2.txt", :t "-1", :o "true", :ref "[:m/id i2]"}
+                       {:e "2", :a ":fil/id", :v "uid:0", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:0", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "deleted.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :remove
-                      [{:e "3", :a ":fil/id", :v "uid:3", :t "1", :o "true", :ref "[:m/id i2]"}
-                       {:e "3", :a ":fil/name", :v "strengt-hemmeleg.txt", :t "1", :o "true", :ref "[:m/id i2]"}
-                       {:e "4", :a ":fil/id", :v "uid:1", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "1", :o "true", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/id", :v "uid:1", :t "2", :o "false", :ref "[:m/id id]"}
-                       {:e "4", :a ":fil/name", :v "secret.txt", :t "2", :o "false", :ref "[:m/id id]"}],
+                      [{:e "2", :a ":fil/id", :v "uid:3", :t "-1", :o "true", :ref "[:m/id i2]"}
+                       {:e "2", :a ":fil/name", :v "strengt-hemmeleg.txt", :t "-1", :o "true", :ref "[:m/id i2]"}
+                       {:e "2", :a ":fil/id", :v "uid:1", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-1", :o "true", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/id", :v "uid:1", :t "-2", :o "false", :ref "[:m/id id]"}
+                       {:e "2", :a ":fil/name", :v "secret.txt", :t "-2", :o "false", :ref "[:m/id id]"}],
               :status "List",
               :tags ["[:delete-file uid:3]" "[:delete-file uid:1]"]}
-             (u/abbr (rh/all-pending-patches conn))))
+             (u/pprint (u/abbr (rh/all-pending-patches conn)))))
       (is (= {:add [], :remove [], :status "Cancelled patch", :tags []}
              (rh/cancel-patch-tag! conn [:delete-file u/uuid-3])))
       (is (= {:add [], :remove [], :status "No changes", :tags []}
